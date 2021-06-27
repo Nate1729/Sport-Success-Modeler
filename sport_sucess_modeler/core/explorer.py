a73c4833d175ee9557745dfbe094c4f7971e9bcf
LethalTedyBear <nferreira1729@gmail.com>
2021-06-27 17:23:38 -0400

    feat: :sparkles: Add heatmap function for covariance matrix

--- sport_sucess_modeler/core/explorer.py
+++ sport_sucess_modeler/core/explorer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def covariance(arr, rowvar=True, sample=True):
 	if rowvar:
@@ -20,3 +21,27 @@ def covariance(arr, rowvar=True, sample=True):
 
 	return arr_out
 
+def cov_heatmap(arr, labels, rowvar=True, sample=True):
+	if rowvar:
+		arr_cov = covariance(arr)
+	else:
+		pass
+
+	fig, ax = plt.subplots()
+	im = ax.imshow(arr)
+
+	# Configure axes
+	ax.set_xticks(np.arange(len(labels)))
+	ax.set_yticks(np.arange(len(labels)))
+	# Label Axes
+	ax.set_xticklabels(labels)
+	ax.set_yticklabels(labels)
+	# Create color bar
+	cbar = ax.figure.colorbar(im, ax=ax)
+
+	fig.tight_layout()
+
+	plt.show()
+
+X = np.random.rand(5,5)
+cov_heatmap(X, ['one', 'two', 'three', 'four', 'five'])
