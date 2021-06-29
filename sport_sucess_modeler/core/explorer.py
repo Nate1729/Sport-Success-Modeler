69e4cc94fc8055b768969652799a41c7ff52d8f9
LethalTedyBear <nferreira1729@gmail.com>
2021-06-28 21:24:32 -0400

    refactor: :recycle: refactor heatmap into function

--- sport_sucess_modeler/core/explorer.py
+++ sport_sucess_modeler/core/explorer.py
@@ -3,30 +3,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+class Explore:
 
-def cov_heatmap(df, title=None):
-	cov = df.cov()	
+	def __init__(self, df_inputs, title):
+		self.inputs = df_inputs
+		self.title = title
 
-	fig, ax = plt.subplots()
-	im = ax.imshow(cov)
+	def heatmap(self):
+		cov = self.inputs.cov()
+		labels = self.inputs.columns
 
-	# Configure axes
-	ax.set_xticks(np.arange(len(labels)))
-	ax.set_yticks(np.arange(len(labels)))
-	# Label Axes
-	ax.set_xticklabels(labels)
-	ax.set_yticklabels(labels)
-	# Create color bar
-	cbar = ax.figure.colorbar(im, ax=ax)
-	# Set title
-	plt.title(title)
-	# Full size figure
-	fig.tight_layout()
+		fig, ax = plt.subplots()
+		im = ax.imshow(cov)
 
-	plt.show()
+		# Configure axes
+		ax.set_xticks(np.arange(len(labels)))
+		ax.set_yticks(np.arange(len(labels)))
+		# Label Axes
+		ax.set_xticklabels(labels)
+		ax.set_yticklabels(labels)
+		# Create color bar
+		cbar = ax.figure.colorbar(im, ax=ax)
+		# Set title
+		plt.title(self.title)
+		# Full size figure
+		fig.tight_layout()
 
-X = np.random.rand(5,5)
-labels = ['one', 'two', 'three', 'four', 'five']
-labels = pd.Index(labels)
-df = pd.DataFrame(X, columns=labels)
-cov_heatmap(df, 'Example Covariance Heatmap')
+		plt.show()
