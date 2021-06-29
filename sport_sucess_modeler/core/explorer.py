01db6d90a4fff93a69421193871e9c3b36747c1e
LethalTedyBear <nferreira1729@gmail.com>
2021-06-28 21:15:52 -0400

    revert: :fire: remove covariance function
    covariance function in pandas will know be used to compute covariance matrix

--- sport_sucess_modeler/core/explorer.py
+++ sport_sucess_modeler/core/explorer.py
@@ -2,25 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def covariance(arr, rowvar=True, sample=True):
-	if rowvar:
-		dim = arr.shape[1]
 
-		arr_out = np.zeros((dim, dim))
-		for i in range(dim):
-			j = 0
-			while j <= i:
-				vec_mean = arr_out.mean(axis=0)
-				arr_out[i, j] = ((arr[:, i] - vec_mean[i])*(arr[:, j] - vec_mean[j])).sum()
-				arr_out[j, i] = arr_out[i, j]
-				j += 1
-
-	if sample:	
-		arr_out /= dim - 1
-	else:
-		arr_out /= dim
-
-	return arr_out
 
 def cov_heatmap(df, title=None):
 	cov = df.cov()	
